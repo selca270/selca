30acda59e3abf6faa8cc20daa7c4af4cb6d1b838
selca270 <selcaministeriodesalud@gmail.com>
2019-11-12 10:47:21 -0300

    Se cargaron los archivos js y css de manera local

---
+++ resources/views/plantilla.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{url ('bootstrap/css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{url ('jquery/css/jquery-ui.css')}}">
+
+        <title>SELCA</title>
+    </head>
+    <body>
+    
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="{{ url('jquery/js/jquery-3.3.1.js') }}"></script>
+        <script src="{{ url('jquery/js/jquery-ui.js') }}" ></script>
+        <script src="{{ url('bootstrap/js/bootstrap.js') }}"></script>
+        <script src="{{ url('bootstrap/js/popper.js') }}"></script>
+    </body>
+</html>
