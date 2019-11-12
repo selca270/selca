1ca5fea06477eab9580413fb4d45167f5d420745
selca270 <selcaministeriodesalud@gmail.com>
2019-11-12 11:02:56 -0300

    Se termino de modificar la plantilla de inicio

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -12,7 +12,21 @@
         <title>SELCA</title>
     </head>
     <body>
-    
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">SELCA </span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="container-fluid">
+            @yield('contenido')
+        </div>
+
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="{{ url('jquery/js/jquery-3.3.1.js') }}"></script>
